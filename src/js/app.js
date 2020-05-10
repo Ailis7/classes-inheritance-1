5150b1ae6ed31e8e52eb9343977ff9e72c171f85
Petr Brovkin <bropet@mail.ru>
2020-05-10 20:57:31 +0300

    final

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+// TODO: write your code here
+import Character from './classes/Character';
+import Bowman from './classes/Bowman';
+import Swordsman from './classes/Swordsman';
+import Magician from './classes/Magician';
+import Undead from './classes/Undead';
+import Zombie from './classes/Zombie';
+import Daemon from './classes/Daemon';
+
+// export default function Character(name, type) {
+// };
+// const man = new Bowman();
+// console.log(man);
